01b450cc67dd7d82ca19d62bb1f1a9c286a7a278
thomas <thomas@dalek>
2013-05-13 15:15:38 -0700

    caching used value

--- src/UsedKeys.hs
+++ src/UsedKeys.hs
@@ -4,11 +4,16 @@ module UsedKeys (
 
 
 import Data.List
+import Hoops
 import Language.Cpp.SyntaxToken
 
 
-usedKeys :: (Eq a) => [SyntaxToken a] -> [Integer]
-usedKeys tokens = case stripPrefix [Identifier "LOOKUP", Punctuation $ punc "("] tokens of
+hlookup :: [SyntaxToken Hoops]
+hlookup = [Identifier "LOOKUP", Punctuation $ punc "("]
+
+
+usedKeys :: [SyntaxToken Hoops] -> [Integer]
+usedKeys tokens = case stripPrefix hlookup tokens of
     Just (Integer n : rest) -> n : usedKeys rest
     Nothing -> case tokens of
         _ : ts -> usedKeys ts
