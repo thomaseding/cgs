fa4058063030f2ce21ba5a0f3f3453b6cc10024b
ThomasEding <thomas@techsoft3d.com>
2013-05-21 14:47:03 -0700

    More powerful matcher. Allows user to selectively capture wild matches

--- src/Match.hs
+++ src/Match.hs
@@ -8,9 +8,9 @@ module Match (
     , Matcher
     , Rest(..)
     , PrefixRest(..)
-    , PrefixWilds(..)
-    , WildsRest(..)
-    , PrefixWildsRest(..)
+    , PrefixCaptures(..)
+    , CapturesRest(..)
+    , PrefixCapturesRest(..)
     , Detailed(..)
     , DetailedRest(..)
     ) where
@@ -39,20 +39,20 @@ data PrefixRest
     deriving (Show)
 
 
-data PrefixWilds
-    = PrefixWilds [SyntaxToken Hoops] [SyntaxToken Hoops]
-    | NoPrefixWilds
+data PrefixCaptures
+    = PrefixCaptures [SyntaxToken Hoops] [SyntaxToken Hoops]
+    | NoPrefixCaptures
 
 
-data WildsRest
-    = WildsRest [SyntaxToken Hoops] [SyntaxToken Hoops]
-    | NoWildsRest
+data CapturesRest
+    = CapturesRest [SyntaxToken Hoops] [SyntaxToken Hoops]
+    | NoCapturesRest
     deriving (Show)
 
 
-data PrefixWildsRest
-    = PrefixWildsRest [SyntaxToken Hoops] [SyntaxToken Hoops] [SyntaxToken Hoops]
-    | NoPrefixWildsRest
+data PrefixCapturesRest
+    = PrefixCapturesRest [SyntaxToken Hoops] [SyntaxToken Hoops] [SyntaxToken Hoops]
+    | NoPrefixCapturesRest
     deriving (Show)
 
 
@@ -98,28 +98,28 @@ instance Match PrefixRest where
             Just (rest, st) -> PrefixRest (DL.toList $ everything st) rest
 
 
-instance Match PrefixWilds where
+instance Match PrefixCaptures where
     match code = let
         m = matchPrim code
         in \ts -> case m ts of
-            Nothing -> NoPrefixWilds
-            Just (_, st) -> PrefixWilds (DL.toList $ everything st) (DL.toList $ wilds st)
+            Nothing -> NoPrefixCaptures
+            Just (_, st) -> PrefixCaptures (DL.toList $ everything st) (DL.toList $ captures st)
 
 
-instance Match WildsRest where
+instance Match CapturesRest where
     match code = let
         m = matchPrim code
         in \ts -> case m ts of
-            Nothing -> NoWildsRest
-            Just (rest, st) -> WildsRest (DL.toList $ wilds st) rest
+            Nothing -> NoCapturesRest
+            Just (rest, st) -> CapturesRest (DL.toList $ captures st) rest
 
 
-instance Match PrefixWildsRest where
+instance Match PrefixCapturesRest where
     match code = let
         m = matchPrim code
         in \ts -> case m ts of
-            Nothing -> NoPrefixWildsRest
-            Just (rest, st) -> PrefixWildsRest (DL.toList $ everything st) (DL.toList $ wilds st) rest
+            Nothing -> NoPrefixCapturesRest
+            Just (rest, st) -> PrefixCapturesRest (DL.toList $ everything st) (DL.toList $ captures st) rest
 
 
 matchPrim :: Code -> [SyntaxToken Hoops] -> Maybe ([SyntaxToken Hoops], MatchState)
@@ -134,7 +134,7 @@ matchPrim code = let
 data MatchState = MatchState {
       wildIndex :: !Int
     , details :: DList (Detailed (SyntaxToken Hoops))
-    , wilds :: DList (SyntaxToken Hoops)
+    , captures :: DList (SyntaxToken Hoops)
     , everything :: DList (SyntaxToken Hoops)
     }
 
@@ -151,7 +151,7 @@ matchM (e:es) (t:ts) = case e of
                 }
             matchM es ts
         else return Nothing
-    Pred p -> case p t of
+    Pred ct p -> case p t of
         Fail -> return Nothing
         Consume -> do
             wildIdx <- gets wildIndex
@@ -159,7 +159,9 @@ matchM (e:es) (t:ts) = case e of
                   wildIndex = wildIndex st + 1
                 , details = details st `DL.snoc` Wild wildIdx t
                 , everything = everything st `DL.snoc` t
-                , wilds = wilds st `DL.snoc` t
+                , captures = case ct of
+                    Capture -> captures st `DL.snoc` t
+                    NoCapture -> captures st
                 }
             matchM es ts
         NoConsume -> matchM es (t:ts)
@@ -169,9 +171,11 @@ matchM (e:es) (t:ts) = case e of
                   wildIndex = wildIndex st + 1
                 , details = details st `DL.snoc` Wild wildIdx t
                 , everything = everything st `DL.snoc` t
-                , wilds = wilds st `DL.snoc` t
+                , captures = case ct of
+                    Capture -> captures st `DL.snoc` t
+                    NoCapture -> captures st
                 }
-            matchM (Pred p':es) ts
+            matchM (Pred ct p':es) ts
 
 
 argsPred :: Int -> SyntaxToken Hoops -> PredResult
@@ -189,33 +193,57 @@ argsPred balance tok = if balance < 0
 
 rawEscapes :: [(String, SyntaxToken Hoops -> PredResult)]
 rawEscapes = [
-      ("$any", \_ -> Consume)
-    , ("$int", \t -> case t of Integer {} -> Consume ; _ -> Fail)
-    , ("$flt", \t -> case t of Floating {} -> Consume ; _ -> Fail)
-    , ("$str", \t -> case t of String {} -> Consume ; _ -> Fail)
-    , ("$var", \t -> case t of Identifier {} -> Consume ; _ -> Fail)
-    , ("$num", \t -> case t of Integer {} -> Consume ; Floating {} -> Consume ; _ -> Fail)
-    , ("$key", \t -> case t of Ext (Key {}) -> Consume ; _ -> Fail)
-    , ("$args", argsPred 0)
+      ("any", \_ -> Consume)
+    , ("int", \t -> case t of Integer {} -> Consume ; _ -> Fail)
+    , ("flt", \t -> case t of Floating {} -> Consume ; _ -> Fail)
+    , ("str", \t -> case t of String {} -> Consume ; _ -> Fail)
+    , ("var", \t -> case t of Identifier {} -> Consume ; _ -> Fail)
+    , ("num", \t -> case t of Integer {} -> Consume ; Floating {} -> Consume ; _ -> Fail)
+    , ("key", \t -> case t of Ext (Key {}) -> Consume ; _ -> Fail)
+    , ("seg", \t -> case t of Ext (SegPath {}) -> Consume ; _ -> Fail)
+    , ("args", argsPred 0)
     ]
 
 
-mangledEscapes :: [(String, SyntaxToken Hoops -> PredResult)]
-mangledEscapes = flip map rawEscapes $ \('$':e, p) -> (mangle e, p)
+data CaptureType
+    = Capture
+    | NoCapture
+    deriving (Show, Eq)
+
+
+toManglePrefix :: CaptureType -> String
+toManglePrefix mt = case mt of
+    Capture -> "eW7jpK"
+    NoCapture -> "VMxKy1"
+
+
+fromManglePrefix :: String -> Maybe CaptureType
+fromManglePrefix str
+    | str == toManglePrefix Capture = Just Capture
+    | str == toManglePrefix NoCapture = Just NoCapture
+    | otherwise = Nothing
+
+
+mangledEscapes :: CaptureType -> [(String, SyntaxToken Hoops -> PredResult)]
+mangledEscapes ct = flip map rawEscapes $ \(e, p) -> (mangle e, p)
     where
-        mangle = ("eW7jpK" ++)
+        manglePrefix = toManglePrefix ct
+        mangle = (manglePrefix ++)
 
 
-rawMangleMap :: [(String, String)]
-rawMangleMap = on zip (map fst) rawEscapes mangledEscapes
+rawMangleMap :: CaptureType -> [(String, String)]
+rawMangleMap ct = on zip (map fst) rawEscapes (mangledEscapes ct)
 
 
 mangleEscapes :: String -> String
 mangleEscapes s = case s of
     "" -> ""
     '$':'$' : rest -> '$' : mangleEscapes rest
-    '$': (span isAlphaNum -> (raw, rest)) -> case lookup ('$':raw) rawMangleMap of
-        Nothing -> '$' : mangleEscapes (raw ++ rest)
+    '$':'!': (span isAlphaNum -> (raw, rest)) -> case lookup raw $ rawMangleMap NoCapture of
+        Nothing -> "$!" ++ mangleEscapes (raw ++ rest)
+        Just mangled -> mangled ++ mangleEscapes rest
+    '$': (span isAlphaNum -> (raw, rest)) -> case lookup raw $ rawMangleMap Capture of
+        Nothing -> "$" ++ mangleEscapes (raw ++ rest)
         Just mangled -> mangled ++ mangleEscapes rest
     c : rest -> c : mangleEscapes rest
 
@@ -224,7 +252,11 @@ lexEquiv :: Code -> [EquivClass]
 lexEquiv code = case runLexer $ mangleEscapes code of
     Left err -> error $ show (code, err)
     Right toks -> flip map toks $ \t -> case t of
-        Identifier name -> maybe (Entity t) Pred $ lookup name mangledEscapes
+        Identifier name -> case lookup name $ mangledEscapes Capture of
+            Just p -> Pred Capture p
+            Nothing -> case lookup name $ mangledEscapes NoCapture of
+                Just p -> Pred NoCapture p
+                Nothing -> Entity t
         _ -> Entity t
 
 
@@ -237,7 +269,7 @@ data PredResult
 
 data EquivClass
     = Entity (SyntaxToken Hoops)
-    | Pred (SyntaxToken Hoops -> PredResult)
+    | Pred CaptureType (SyntaxToken Hoops -> PredResult)
 
 
 
