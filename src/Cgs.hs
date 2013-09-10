5a163685959822ca543a92ee652025c9257ed282
Thomas Eding <thomas@techsoft3d.com>
2013-09-09 17:38:24 -0700

    Support multiple file input. Also added support for chunking output to specified statement count.

--- src/Cgs.hs
+++ src/Cgs.hs
@@ -1,18 +1,27 @@
+{-# LANGUAGE ViewPatterns #-}
+
 module Cgs (
     main
 ) where
 
 
 import Cgs.Args (parseArgsIO, CgsOptions(..))
+import Control.Exception (assert)
+import Control.Monad ((<=<))
 import Control.Monad.Identity (runIdentity)
+import Control.Monad.ListM (takeWhileM, groupByM)
+import Control.Monad.State.Lazy (evalState, modify, gets, get, put)
+import Data.List (isPrefixOf)
 import Data.Tagged (untag)
 import Hoops.Lex (runLexer)
+import Hoops.Match
 import Hoops.SyntaxToken
 import Language.Cpp.Pretty (pretty)
 import System.Directory (getCurrentDirectory, createDirectoryIfMissing)
 import System.Exit (exitFailure)
 import System.FilePath ((</>))
-import Text.Indent (indent, IndentMode(KeepOldTabs), Tagged, CodeGen)
+import Text.Indent (indent, IndentMode(DropOldTabs), Tagged, CodeGen)
+import Text.Parsec (ParseError)
 import Transform.Expand (expand)
 import Transform.Extract (extract, ExtractOptions)
 import Transform.Flatten (flatten)
@@ -24,20 +33,81 @@ main :: IO ()
 main = do
     mCgsOpts <- parseArgsIO
     case mCgsOpts of
-        Nothing -> badArgs
-        Just cgsOpts -> do
+        Left err -> badArgs err
+        Right cgsOpts -> do
             let getOpt f = runIdentity $ f cgsOpts
-            toks <- readFile (getOpt cgsFile) >>= lexCode
-            toks' <- runExtract (cgsExtractOpts cgsOpts) toks
-            putStrLn $ additionalIndent $ pretty expandHoops $ cgs toks'
+            tokss <- mapM (lexCode <=< readFile) $ getOpt cgsFiles
+            let content = catMainContent tokss
+            content' <- runExtract (cgsExtractOpts cgsOpts) content
+            let cgsContent = cgs content'
+                cgsContents = chunkStmts (cgsChunkSize cgsOpts) cgsContent
+                tus = zipWith compileTranslationUnit [0 ..] cgsContents
+            mapM_ (putStrLn . reindent . pretty expandHoops) tus
 
 
-badArgs :: IO ()
-badArgs = putStrLn "Could not parse command line arguments"
+badArgs :: ParseError -> IO ()
+badArgs _ = putStrLn "Could not parse command line arguments"
 
 
-additionalIndent :: String -> String
-additionalIndent = untag . (indent KeepOldTabs :: String -> Tagged CodeGen String)
+compileTranslationUnit :: Int -> [SyntaxToken Hoops] -> [SyntaxToken Hoops]
+compileTranslationUnit unitId tokens = prelude ++ tokens ++ prologue
+    where
+        code = either (const $ assert False undefined) id . runLexer
+        codeChain = "code_chain_" ++ show unitId
+        prelude = code $ unlines $ [
+            "#include \"cgs.h\"\n",
+            "int " ++ codeChain ++ " () {\n" ]
+        prologue = code $ unlines [
+            "return 0;",
+            "}\n" ]
+
+
+data ChunkState = ChunkState {
+    consumedStmts :: Int,
+    braceBalance :: Int
+}
+
+
+chunkByM :: (Monad m) => (a -> m Bool) -> [a] -> m [[a]]
+chunkByM _ [] = return []
+chunkByM p (x:xs) = do
+    keep <- p x
+    xss <- chunkByM p xs
+    let xss' = case xss of
+            ys : yss -> (x:ys) : yss
+            [] -> [[x]]
+    return $ if keep
+        then xss'
+        else [] : xss'
+
+
+chunkStmts :: Int -> [SyntaxToken Hoops] -> [[SyntaxToken Hoops]]
+chunkStmts chunkSize = flip evalState st . chunkByM chunker
+    where
+        st = ChunkState {
+            consumedStmts = 0,
+            braceBalance = 0 }
+        chunker token = do
+            balance <- gets braceBalance
+            numConsumed <- gets consumedStmts
+            placeInCurrChunk <- if balance == 0 && numConsumed >= chunkSize
+                then do
+                    modify $ \st -> st { consumedStmts = 0 }
+                    return False
+                else return True
+            case token of
+                Punctuation (unpunc -> ";") -> do
+                    modify $ \st -> st { consumedStmts = consumedStmts st + 1 }
+                Punctuation (unpunc -> "{") -> do
+                    modify $ \st -> st { braceBalance = braceBalance st + 1 }
+                Punctuation (unpunc -> "}") -> do
+                    modify $ \st -> st { braceBalance = braceBalance st - 1 }
+                _ -> return ()
+            return placeInCurrChunk
+
+
+reindent :: String -> String
+reindent = untag . (indent DropOldTabs :: String -> Tagged CodeGen String)
 
 
 runExtract :: ExtractOptions -> [SyntaxToken Hoops] -> IO [SyntaxToken Hoops]
@@ -68,6 +138,53 @@ lexCode :: Code -> IO [SyntaxToken Hoops]
 lexCode code = case runLexer code of
     Left err -> print err >> exitFailure
     Right ts -> return ts
+
+
+catMainContent :: [[SyntaxToken Hoops]] -> [SyntaxToken Hoops]
+catMainContent = concatMap extractMainContent
+
+
+extractMainContent :: [SyntaxToken Hoops] -> [SyntaxToken Hoops]
+extractMainContent = let
+    mainFunc = match "int main ($!args) {"
+    codeChainSig = match "int $var ($!args) {"
+    codeChainVar = isPrefixOf "code_chain_"
+    codeChainFunc tokens = case tokens of
+        (codeChainSig -> CapturesRest [Identifier (codeChainVar -> True)] rest) -> Rest rest
+        _ -> NoRest
+    dropBoringDecls = match $ concat [
+        "char string_buffer[256];",
+        "float ff;",
+        "int ii;",
+        "long ll;",
+        "HC_KEY key;",
+        "float matrix[16];" ]
+    go tokens = case extractBody tokens of
+        (dropBoringDecls -> Rest rest) -> rest
+        ts -> ts
+    in \tokens -> case tokens of
+        (mainFunc -> Rest rest) -> go rest
+        (codeChainFunc -> Rest rest) -> go rest
+        _ : rest -> extractMainContent rest
+        [] -> []
+
+
+extractBody :: [SyntaxToken Hoops] -> [SyntaxToken Hoops]
+extractBody = flip evalState 0 . takeWhileM f
+    where
+        f token = case token of
+            Punctuation (unpunc -> sym) -> case sym of
+                "{" -> modify (+ 1) >> return True
+                "}" -> modify (subtract 1) >> gets (>= 0)
+                _ -> return True
+            Keyword (unkw -> name) -> return $ case name of
+                "return" -> False
+                _ -> True
+            Identifier name -> return $ not $ "code_chain_" `isPrefixOf` name
+            _ -> return True
+
+
+
 
 
 
--- src/Cgs/Args.hs
+++ src/Cgs/Args.hs
@@ -7,45 +7,122 @@ module Cgs.Args (
 
 import Control.Monad.Identity (Identity(Identity))
 import Control.Monad.State (State, execState, modify)
+import Data.Char (isDigit)
+import Data.Function (on)
+import Data.List (groupBy, sortBy)
 import System.Environment (getArgs)
+import System.FilePath (takeBaseName)
+import Text.Parsec hiding (satisfy)
 import Transform.Extract (ExtractOptions(..))
 
 
-data CgsOptions f = CgsOptions {
-    cgsFile :: f FilePath,
+data CgsOptions a = CgsOptions {
+    cgsFiles :: a [FilePath],
+    cgsChunkSize :: Int,
     cgsExtractOpts :: ExtractOptions }
 
 
-type ParseState = CgsOptions Maybe
+type Parser a = Parsec [String] (CgsOptions Maybe) a
 
 
-collapse :: ParseState -> Maybe (CgsOptions Identity)
+collapse :: (CgsOptions Maybe) -> Maybe (CgsOptions Identity)
 collapse CgsOptions {
-    cgsFile = Just file,
+    cgsFiles = Just files,
+    cgsChunkSize = chunkSize,
     cgsExtractOpts = opts } = Just $ CgsOptions {
-        cgsFile = Identity file,
+        cgsFiles = Identity files,
+        cgsChunkSize = chunkSize,
         cgsExtractOpts = opts }
 collapse _ = Nothing
 
 
-parseArgsIO :: IO (Maybe (CgsOptions Identity))
-parseArgsIO = fmap parseArgs getArgs
+parseArgsIO :: IO (Either ParseError (CgsOptions Identity))
+parseArgsIO = fmap runParseArgs getArgs
 
 
-parseArgs :: [String] -> Maybe (CgsOptions Identity)
-parseArgs = collapse . flip execState st . parseArgsM
+runParseArgs :: [String] -> Either ParseError (CgsOptions Identity)
+runParseArgs = runParser parseArgs st source
     where
+        source = ""
         st = CgsOptions {
-            cgsFile = Nothing,
+            cgsFiles = Nothing,
+            cgsChunkSize = 4000,
             cgsExtractOpts = extractOpts }
         extractOpts = ExtractOptions {
             minStmtRequirement = 0 }
 
 
-parseArgsM :: [String] -> State ParseState ()
-parseArgsM args = case args of
-    [file] -> modify $ \st -> st { cgsFile = Just file }
-    _ -> return ()
+parseArgs :: Parser (CgsOptions Identity)
+parseArgs = do
+    parseTaggedOpts
+    parseUntaggedOpts
+    mState <- fmap collapse getState
+    case mState of
+        Nothing -> parserFail "Missing arguments"
+        Just state -> return state
+
+
+parseTaggedOpts :: Parser ()
+parseTaggedOpts = return ()
+
+
+parseUntaggedOpts :: Parser ()
+parseUntaggedOpts = parseFiles
+
+
+parseFiles :: Parser ()
+parseFiles = do
+    files <- many1 anyArg <?> "Missing input files"
+    let files' = sortBy (humanOrdering `on` takeBaseName) files
+    modifyState $ \st -> st { cgsFiles = Just files' }
+
+
+data Human
+    = NonDigit Char
+    | Number Int
+    deriving (Show, Eq, Ord)
+
+
+toHuman :: [Char] -> [Human]
+toHuman str = case span isDigit str of
+    (ds, rest) -> case rest of
+        c : cs -> case ds of
+            [] -> NonDigit c : toHuman cs
+            _ -> Number (read ds) : toHuman rest
+        [] -> case ds of
+            [] -> []
+            _ -> [Number $ read ds]
+
+
+humanOrdering :: String -> String -> Ordering
+humanOrdering s1 s2 = compare h1 h2
+    where
+        grouper (NonDigit _) (NonDigit _) = True
+        grouper (Number _) (Number _) = True
+        grouper _ _ = False
+        massage = groupBy grouper . toHuman
+        h1 = massage s1
+        h2 = massage s2
+
+
+updatePosArg :: SourcePos -> String -> SourcePos
+updatePosArg pos _ = incSourceColumn pos 1
+
+
+satisfy :: (String -> Bool) -> Parser String
+satisfy f = tokenPrim showArg nextPos testArg
+    where
+        showArg = show
+        testArg arg = if f arg then Just arg else Nothing
+        nextPos pos arg args = updatePosArg pos arg
+
+
+anyArg :: Parser String
+anyArg = satisfy $ const True
+
+
+
+
 
 
 
